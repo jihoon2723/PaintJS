563892a1d70b4f41907549e12219bff3f3937631
이지훈 <59809529+jihoon2723@users.noreply.github.com>
2020-03-11 13:40:32 +0900

    brushColor Event
    선 색깔 변경 이벤트 설정

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
 
 canvas.width = 700;
-canvas.height = 700;
+canvas.height = 700;   //<<pixel modifier 을 줘야함 그래야지 화면에 원하는 위치에 제대로 그려짐
 
 ctx.strokeStyle="#2c2c2c";
 ctx.lineWidth=2.5;
@@ -23,7 +24,7 @@ function onMouseMove(event){
     const x = event.offsetX;
     const y = event.offsetY;
     if(!painting){
-        ctx.beginPath();
+        ctx.beginPath();  //path 는 선
         ctx.moveTo(x,y);
     }else{
         ctx.lineTo(x,y);
@@ -31,10 +32,11 @@ function onMouseMove(event){
     }
 }
 
-function onMouseDown(event){
-    painting = true;
-}
 
+function handleColorClick(event){
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle= color;
+}
 
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
@@ -42,3 +44,5 @@ if(canvas){
     canvas.addEventListener("mouseup",stopPainting);
     canvas.addEventListener("mouseleave",stopPainting);
 }
+
+Array.from(colors).forEach(color=>color.addEventListener("click",handleColorClick))
