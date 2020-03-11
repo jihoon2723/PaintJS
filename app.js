7c239e7a5570787d786441eff79f5d9c595d9ce3
이지훈 <59809529+jihoon2723@users.noreply.github.com>
2020-03-11 14:22:52 +0900

    Filling Event
    전체 화면을 체우는 이벤트 적용

--- app.js
+++ app.js
@@ -2,16 +2,23 @@ const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
+const INITIAL_COLOR="#2c2c2c";
+const CANVAS_SIZE= 700;
 
-canvas.width = 700;
-canvas.height = 700;   //<<pixel modifier 을 줘야함 그래야지 화면에 원하는 위치에 제대로 그려짐
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;   //<<pixel modifier 을 줘야함 그래야지 화면에 원하는 위치에 제대로 그려짐
 
-ctx.strokeStyle="#2c2c2c";
+ctx.strokeStyle="INITIAL_COLOR";
+ctx.fillStyle="INITIAL_COLOR";
+
 ctx.lineWidth=2.5;
 
 
+
 let painting = false;
+let filling = false;
 
 function stopPainting(){
     painting=false;
@@ -38,6 +45,7 @@ function onMouseMove(event){
 function handleColorClick(event){
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle= color;
+    ctx.fillStyle=color;
 }
 
 function handleRangeChange(event){
@@ -45,12 +53,28 @@ function handleRangeChange(event){
     ctx.lineWidth = size;
 }
 
+function handleModeClick(){
+    if(filling===true){
+        filling=false;
+        mode.innerText="Fill"
+    }else{
+        filling =true;
+        mode.innerText="Paint"
+        
+    }
+}
 
+function handelCanvasClick(){
+    if(filling){
+    ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE);
+}
+}
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown",startPainting);
     canvas.addEventListener("mouseup",stopPainting);
     canvas.addEventListener("mouseleave",stopPainting);
+    canvas.addEventListener("click",handelCanvasClick);
 }
 
 Array.from(colors).forEach(color=>color.addEventListener("click",handleColorClick));
@@ -59,4 +83,9 @@ Array.from(colors).forEach(color=>color.addEventListener("click",handleColorClic
 
 if(range){
     range.addEventListener("input", handleRangeChange);
+}
+
+
+if(mode){
+    mode.addEventListener("click",handleModeClick);
 }
