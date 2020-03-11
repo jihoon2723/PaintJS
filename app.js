ad2e9aacf29412d0713fd480932be8d258259711
이지훈 <59809529+jihoon2723@users.noreply.github.com>
2020-03-11 14:47:03 +0900

    saveEvent
    png이미지로저장하는 기능 추가

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
 const INITIAL_COLOR="#2c2c2c";
 const CANVAS_SIZE= 700;
@@ -10,6 +11,8 @@ const CANVAS_SIZE= 700;
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;   //<<pixel modifier 을 줘야함 그래야지 화면에 원하는 위치에 제대로 그려짐
 
+ctx.fillStyle = "white";
+ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE); //기본배경을 하얀 배경으로 설정
 ctx.strokeStyle="INITIAL_COLOR";
 ctx.fillStyle="INITIAL_COLOR";
 
@@ -69,12 +72,26 @@ function handelCanvasClick(){
     ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE);
 }
 }
+
+function handleCM(event){
+    event.preventDefault(); //우클릭 목록 막음
+}
+
+function handleSaveClick(){
+     const image=canvas.toDataURL("image/png");
+     const link = document.createElement("a");
+     link.href = image;
+     link.download="paintJS[🥰]";
+     link.click();
+}
+
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown",startPainting);
     canvas.addEventListener("mouseup",stopPainting);
     canvas.addEventListener("mouseleave",stopPainting);
     canvas.addEventListener("click",handelCanvasClick);
+    canvas.addEventListener("contextmenu",handleCM);
 }
 
 Array.from(colors).forEach(color=>color.addEventListener("click",handleColorClick));
@@ -88,4 +105,8 @@ if(range){
 
 if(mode){
     mode.addEventListener("click",handleModeClick);
+}
+
+if(saveBtn){
+    saveBtn.addEventListener("click",handleSaveClick);
 }
